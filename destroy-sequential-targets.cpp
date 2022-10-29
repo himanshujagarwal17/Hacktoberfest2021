5af5e878c0d33f5cdbbfb90a28d3c05f1b17e48b
himanshujagarwal17 <116967998+himanshujagarwal17@users.noreply.github.com>
2022-10-29 21:54:02 +0530

    Create destroy-sequential-targets.cpp

---
+++ destroy-sequential-targets.cpp
@@ -0,0 +1,49 @@
+//Name: Himanshu Jagarwal
+//Language: C++
+
+class Solution {
+public:
+    int destroyTargets(vector<int>& a, int d) {
+            sort(a.begin(),a.end());
+         map<int,int>m;
+            for(int i=0;i<a.size();i++)
+            {
+                    int x = a[i]/d;
+                    x = a[i]-x*d;
+                    if(x==0) x=d;
+                    m[x]++;
+                   
+            }
+            int mx=-1;
+            for(auto i: m)
+            {
+                    if(i.second>mx)
+                            mx = i.second;
+            }
+           set<int>ans;
+            for(auto i : m)
+            {
+                    if(i.second==mx)
+                            ans.insert(i.first);
+            }
+            int mn = INT_MAX;
+             for(int i=0;i<a.size();i++)
+            {
+                    int x = a[i]/d;
+                    x = a[i]-x*d;
+                    if(x==0) x=d;
+                   
+                    if(ans.find(x)!=ans.end())
+                    {
+                            if(mn>a[i])
+                                    mn =a[i];
+                    }
+                   
+            }
+            
+            return(mn);            
+                    
+            
+        
+    }
+};
